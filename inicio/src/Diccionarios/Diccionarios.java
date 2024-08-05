4da9642a7433b65036386bf43721057e3e2be5e8
karlinux <karlinux47@gmail.com>
2024-08-05 18:12:38 +0200

    Lists finished and new diccionaries started and finished

---
+++ inicio/src/Diccionarios/Diccionarios.java
@@ -0,0 +1,38 @@
+package Diccionarios;
+
+import java.util.HashMap;
+
+public class Diccionarios {
+   public static void main(String[] args){
+       System.out.println("#### Estudiamos los diccionarios ####");
+       System.out.println();
+       System.out.println(" ## Metodos para los diccionarios: ##");
+       System.out.println(" # put: Añade elementos, palabra clave + elemento");
+       System.out.println(" # get: Obtiene el valor pasando la clave");
+       System.out.println(" # size: Numero de elementos`");
+       System.out.println(" # containsKey: devuelve true/false si el diccionario tiene algun \n elemento con la clave indicada");
+       System.out.println(" # containsValue: devuelve true/false si el diccionario contiene el valor indicado");
+       System.out.println(" # remove: eleimina el valor de la clave indicada");
+       System.out.println(" # clear: elimina todos los elementos del diccionario");
+       System.out.println();
+       HashMap<String,String> diasIngles = new HashMap<String, String>();
+       System.out.println(" Creamos diccionario diasIngles y insertamos los dias de la semana\n diasIngles.put('diasCastellano', 'diasenIngles')");
+       diasIngles.put("Lunes", "Monday");
+       diasIngles.put("Martes", "Tuesday");
+       diasIngles.put("Miercoles", "Wednesday");
+       diasIngles.put("Jueves","Thursday");
+       diasIngles.put("Viernes", "Friday");
+       diasIngles.put("Sabado", "Saturday");
+       diasIngles.put("Domingo", "Sunday");
+       System.out.println(" Tamaño del diccionario: " + diasIngles.size() );
+       System.out.println(" Esta el viernes??: containsKey('Viernes') -->" + diasIngles.containsKey("Viernes"));
+       System.out.println(" Esta el Sunday??: containsVelue('Sunday') -->" + diasIngles.containsValue("Sunday"));
+       System.out.println("Eliminamos el domingo y preguntamos si esiste el valor del Sunday");
+       diasIngles.remove("Domingo");
+       System.out.println(" Esta el Sunday: -->" + diasIngles.containsValue("Sunday"));
+       System.out.println();
+       System.out.println(" Borramos el diccionario: clear");
+       diasIngles.clear();
+       System.out.println(" Tamaño del diccionario: " + diasIngles.size());
+   }
+}
--- inicio/src/Listas/Listas.java
+++ inicio/src/Listas/Listas.java
@@ -22,5 +22,23 @@ public class Listas {
     System.out.println("Modificamos la posicion 0 por 10 y la 1 por 11");
     System.out.println("Llamamos a la posicion 0 con numeros.get(0)\n" + numeros.get(0));
     System.out.println("Llamamos a la posicion 1 con numeros.get(1)\n" + numeros.get(1));
+    System.out.println();
+    System.out.println("Tamaño de la lista: " + numeros.size());
+    System.out.println();
+    numeros.add(0,20);
+    System.out.println("Añadimos el en la posicion 0 el numero 20: numeros.add(0,20)");
+    System.out.println("Tamaño de la lista: " + numeros.size());
+    System.out.println("Posicion 0 de la lista: " + numeros.get(0));
+    System.out.println();
+    System.out.println("Vemos contains remove y clear");
+    System.out.println("REMOVE: numeros.remove(0) para la posicion y Integer.valueOf(3) para eliminar la primera ocurrencia");
+    System.out.println("Removemos la posicion 0 es decir el 20: numeros.remove(0);");
+    numeros.remove(0);
+    System.out.println("Tamaños de la lista: " + numeros.size());
+    System.out.println("Numeros: " + numeros.get(0) + ", " + numeros.get(1) +", "+ numeros.get(2) + ", " + numeros.get(3));
+    System.out.println("Eliminamos el numero 11: numeros.remove(Integer.valueOf(11));");
+    numeros.remove(Integer.valueOf(11));
+    System.out.println("Tamaños de la lista: " + numeros.size());
+    System.out.println("Numeros: " + numeros.get(0) + ", " + numeros.get(1) +", "+ numeros.get(2));
   }
 }
